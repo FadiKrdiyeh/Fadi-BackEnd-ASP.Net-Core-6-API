c0ec00d8dae52b877a4566cb8c085ed23e5d4e40
FadiKrdiyeh <66fadiiiiii66@gmail.com>
2023-02-13 20:58:49 +0200

    Create two empty controllers(Administration - User)

---
+++ FadiBackEndApI/Controllers/AdministrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FadiBackEndApI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministrationController : ControllerBase
+    {
+    }
+}
